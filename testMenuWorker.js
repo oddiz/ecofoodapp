e498238ce0f8223aefd24dc8dc819e942665a548
Kaan Şarkaya <kaansarkaya@gmail.com>
2022-03-28 12:28:20 +0300

    chore: formatting and calculate SP timing

--- testMenuWorker.js
+++ testMenuWorker.js
@@ -1,5 +1,5 @@
-onmessage = function(e) {
-    var activeMenuW, rollNumberW,foodCountW,budgetW, calorieW, caloriePerDollarW, maxSpW, stomachContentW,option;
+onmessage = function (e) {
+    var activeMenuW, rollNumberW, foodCountW, budgetW, calorieW, caloriePerDollarW, maxSpW, stomachContentW, option;
     if (e.data.origin !== "ecoFood") {
         return;
     }
@@ -12,16 +12,18 @@ onmessage = function(e) {
     maxSpW = e.data.maxSpInput || Infinity;
     stomachContentW = e.data.stomachContent;
     option = e.data.simType;
-    
 
-    testMenuWorker(activeMenuW,rollNumberW,foodCountW,budgetW, calorieW, caloriePerDollarW, maxSpW, stomachContentW,option);
+
+    testMenuWorker(activeMenuW, rollNumberW, foodCountW, budgetW, calorieW, caloriePerDollarW, maxSpW, stomachContentW, option);
 };
 
 
 
 
 function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie, caloriePerDollar, maxSp, stomachContent, option) {
-	//randomizes and tests the active menu array
+
+    console.time("SPcalculation");
+    //randomizes and tests the active menu array
     "use strict";
 
     if (calorie !== Infinity) {
@@ -34,46 +36,49 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
         maxSp = parseInt(maxSp);
     }
 
-	function calculateSP(menu) {
-		//accepts an array of food objects
-        
-		var baseGain = 12;
-		var totalCarb = 0;
-		var totalProtein = 0;
-		var totalFat = 0;
-		var totalVitamin = 0;
-		var totalCal = 0;
+
+    function calculateSP(menu) {
+        //accepts an array of food objects
+
+        var baseGain = 12;
+        var totalCarb = 0;
+        var totalProtein = 0;
+        var totalFat = 0;
+        var totalVitamin = 0;
+        var totalCal = 0;
         var foodList = "";
 
-		for (var i = 0; i < menu.length; i++) {
+        for (var i = 0; i < menu.length; i++) {
             if (menu[i].cal == 0) {
                 menu[i].cal = 1;
             }
-			totalCal += parseInt(menu[i].cal);
-			totalCarb += parseInt(menu[i].cal) * parseInt(menu[i].carb);
-			totalProtein += parseInt(menu[i].cal) * parseInt(menu[i].pro);
-			totalFat += parseInt(menu[i].cal) * parseInt(menu[i].fat);
-			totalVitamin += parseInt(menu[i].cal) * parseInt(menu[i].vit);
-			foodList = foodList + menu[i].name + "+";
-		}
-		var totalTotal = totalCarb + totalProtein + totalFat + totalVitamin;
+            totalCal += parseInt(menu[i].cal);
+            totalCarb += parseInt(menu[i].cal) * parseInt(menu[i].carb);
+            totalProtein += parseInt(menu[i].cal) * parseInt(menu[i].pro);
+            totalFat += parseInt(menu[i].cal) * parseInt(menu[i].fat);
+            totalVitamin += parseInt(menu[i].cal) * parseInt(menu[i].vit);
+            foodList = foodList + menu[i].name + "+";
+        }
+        var totalTotal = totalCarb + totalProtein + totalFat + totalVitamin;
 
-		var totalAverage = totalTotal / totalCal;
+        var totalAverage = totalTotal / totalCal;
 
-		var maxTotal = Math.max(totalCarb, totalProtein, totalFat, totalVitamin);
+        var maxTotal = Math.max(totalCarb, totalProtein, totalFat, totalVitamin);
 
-		var balancedMultiplier = (totalTotal / (maxTotal * 4)) * 2;
+        var balancedMultiplier = (totalTotal / (maxTotal * 4)) * 2;
 
-		return {
-			SP: baseGain + (totalAverage * balancedMultiplier),
-			foodList: foodList,
-			multiplier: balancedMultiplier
-		};
-	}
-    
-    
+        var result = {
+            SP: baseGain + (totalAverage * balancedMultiplier),
+            foodList: foodList,
+            multiplier: balancedMultiplier
+        };
+
+        return result
+    }
+
+
     var getMenu = function () {
-        
+
         return {
             //returns array of foods
             all: activeMenuArray.concat(stomachContent),
@@ -81,11 +86,11 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
             active: activeMenuArray
         };
     };
-    
-	var randomizer = 0;
-	var bestMenuNames;
-	var bestIndex = 0;
-	var bestMultiplier = 0;
+
+    var randomizer = 0;
+    var bestMenuNames;
+    var bestIndex = 0;
+    var bestMultiplier = 0;
     var bestSP = 0;
     var bestTotalPrice = 0;
     var bestTotalCalorie = 0;
@@ -93,26 +98,26 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
     var bestMenuStomachArray = getMenu().stomach;
     var bestMenuNonStomachArray = [];
     var totalIterations;
-	//console.log("usedFoods check " + activeMenuArray);
+    //console.log("usedFoods check " + activeMenuArray);
 
     var progressPercent = 0;
-    var progressPercentOld= 0;
+    var progressPercentOld = 0;
     var totalPrice = 0;
     var totalCalorie = 0;
-    
-    if (option === "random" || !option){
+
+    if (option === "random" || !option) {
         totalIterations = rollNumber;
         console.log("Starting random");
         for (var i = 0; i <= rollNumber; i++) {
             var randomMenu = [];
 
-            
-            
+
+
             totalPrice = 0;
             totalCalorie = 0;
-    
+
             if (option === "random") {
-    
+
                 for (var q = 0; q < foodCount; q++) {
                     randomizer = Math.floor(Math.random() * activeMenuArray.length);
                     randomMenu.push(activeMenuArray[randomizer]);
@@ -121,33 +126,33 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
                 }
             }
 
-            
-    
-    
-            progressPercent = Math.floor(i/rollNumber * 100);
-    
+
+
+
+            progressPercent = Math.floor(i / rollNumber * 100);
+
             if (progressPercent !== progressPercentOld) {
-                
+
                 progressPercentOld = progressPercent;
-                
-    
+
+
                 postMessage({
                     type: "progress_percent",
                     percentage: progressPercent
                 });
-    
+
             }
-    
-            if(
-                ((parseFloat(budget) != -1) && totalPrice > parseFloat(budget)) || 
+
+            if (
+                ((parseFloat(budget) != -1) && totalPrice > parseFloat(budget)) ||
                 ((parseInt(calorie) != -1) && totalCalorie > parseInt(calorie)) ||
-                (((totalCalorie/totalPrice) < caloriePerDollar))
+                (((totalCalorie / totalPrice) < caloriePerDollar))
             ) {
                 randomMenu = [];
                 continue;
             }
             var result = calculateSP(randomMenu.concat(getMenu().stomach));
-            
+
             if (result.SP > maxSp) {
                 randomMenu = [];
                 continue;
@@ -160,48 +165,50 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
                 bestTotalPrice = totalPrice;
                 bestTotalCalorie = totalCalorie;
                 bestMenuArray = randomMenu.concat(getMenu().stomach);
-                bestMenuNonStomachArray = randomMenu; 
+                bestMenuNonStomachArray = randomMenu;
             }
-            
-            
-    
-            
-            
-            
+
+
+
+
+
+
         }
     }
 
     if (option === "definitive") {
         console.log("Starting definitive");
         calculateAllIterations();
+        console.timeEnd("SPcalculation");
     }
-    
-    function calculateAllIterations () {
+
+    function calculateAllIterations() {
 
         var inputMenu = getMenu().active;
         var items = parseInt(foodCount);
         var groups = inputMenu.length;
-        totalIterations = (factorial(items+groups-1)) / (factorial(items)*factorial(groups-1));
+        totalIterations = (factorial(items + groups - 1)) / (factorial(items) * factorial(groups - 1));
         totalIterations = Math.round(totalIterations);
         var counter = 0;
-        
+
         partiteIdentical(items, groups);
+
         function partiteIdentical(items, groups, args = [0], index = 0) {
 
             if (groups === 0) {
-                
-                var argsTotal = args.reduce(function(a,b) { 
-                    return a+b; 
+
+                var argsTotal = args.reduce(function (a, b) {
+                    return a + b;
                 });
                 if (argsTotal === items) {
-                    
+
                     var definitiveMenu = constructMenuFromArgs(args, getMenu().stomach);
-                    
-                    if(totalCalorie == 0 && totalPrice == 0) {
+
+                    if (totalCalorie == 0 && totalPrice == 0) {
                         totalCalorie = 1;
                     }
 
-                    if((totalPrice <= budget) && (totalCalorie <= calorie) && ((totalCalorie/totalPrice) >= caloriePerDollar)) {
+                    if ((totalPrice <= budget) && (totalCalorie <= calorie) && ((totalCalorie / totalPrice) >= caloriePerDollar)) {
                         var result = calculateSP(definitiveMenu.all);
                         if (result.SP > bestSP && result.SP <= maxSp) {
                             bestSP = result.SP;
@@ -209,7 +216,7 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
                             bestIndex = counter;
                             bestMultiplier = result.multiplier;
                             bestTotalPrice = totalPrice;
-                            bestTotalCalorie = totalCalorie;                            
+                            bestTotalCalorie = totalCalorie;
                             bestMenuArray = definitiveMenu.all;
                             bestMenuNonStomachArray = definitiveMenu.nonStomach;
                             postMessage({
@@ -222,7 +229,7 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
                                     foodQty: parseInt(foodCount) + stomachContent.length,
                                     totalPrice: bestTotalPrice,
                                     totalCalorie: bestTotalCalorie,
-                                    caloriePerDollar: bestTotalCalorie/bestTotalPrice,
+                                    caloriePerDollar: bestTotalCalorie / bestTotalPrice,
                                     resultMenuArray: bestMenuArray,
                                     totalIterations: totalIterations,
                                     resultMenuStomach: processMenuNames(definitiveMenu.stomach),
@@ -232,37 +239,37 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
 
                         }
                     }
-                                        
-                    counter += 1;                    
-                    progressPercent = Math.floor(counter/totalIterations * 100);
-                    
+
+                    counter += 1;
+                    progressPercent = Math.floor(counter / totalIterations * 100);
+
                     if (progressPercent !== progressPercentOld) {
-                        
+
                         progressPercentOld = progressPercent;
                         postMessage({
                             type: "progress_percent",
                             percentage: progressPercent
                         });
-                        
+
                     }
-                    
+
                 }
-                    
+
             } else {
-                
-                var groupRest = groups-1;        
-                
-                for (args[index] = 0; args[index] < items+1; ++args[index]) {
-                    
+
+                var groupRest = groups - 1;
+
+                for (args[index] = 0; args[index] < items + 1; ++args[index]) {
+
                     partiteIdentical(items, groupRest, args, index + 1);
-            
+
                 }
             }
         }
 
-        
-    
-        function constructMenuFromArgs(args,stomach) {
+
+
+        function constructMenuFromArgs(args, stomach) {
             //[3,0,2,3]
             if (!stomach) {
                 stomach = [];
@@ -270,18 +277,18 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
 
             totalCalorie = 0;
             totalPrice = 0;
-            
+
             var calculateMenu = [];
-    
-            args.forEach(function(ele,index) {
-    
+
+            args.forEach(function (ele, index) {
+
                 for (var i = 0; i < ele; i++) {
                     calculateMenu.push(inputMenu[index]);
                     totalPrice += parseFloat(inputMenu[index].price);
                     totalCalorie += parseInt(inputMenu[index].cal);
                 }
             });
-            
+
             return {
                 all: calculateMenu.concat(stomach),
                 stomach: stomach,
@@ -289,15 +296,15 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
             };
         }
 
-        function factorial (number) {
+        function factorial(number) {
             var result = 1;
             for (var i = 2; i <= number; i++) {
                 result *= i;
             }
-            
+
             return result;
         }
-    
+
     }
 
     function processMenuNames(array) {
@@ -335,7 +342,7 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
         var foodName = "";
         for (var b = 0; b < arrayNames.length; b++) {
             foodName = arrayNames[b];
-    
+
             if (finalResult[foodName] >= 0) {
                 finalResult[foodName] += 1;
             } else {
@@ -345,8 +352,8 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
 
         return finalResult;
     }
-    if(bestMenuNames) {
-
+    if (bestMenuNames) {
+        console.timeEnd("SPcalculation");
         /*
          *console.log(bestSP + " found at " + bestIndex + ". try.");
          */
@@ -360,19 +367,21 @@ function testMenuWorker(activeMenuArray, rollNumber, foodCount, budget, calorie,
                 foodQty: parseInt(foodCount) + stomachContent.length,
                 totalPrice: bestTotalPrice,
                 totalCalorie: bestTotalCalorie,
-                caloriePerDollar: bestTotalCalorie/bestTotalPrice,
+                caloriePerDollar: bestTotalCalorie / bestTotalPrice,
                 resultMenuArray: bestMenuArray,
                 totalIterations: totalIterations,
-                resultMenuStomach: processMenuNames(bestMenuStomachArray)|| {test: 2},
+                resultMenuStomach: processMenuNames(bestMenuStomachArray) || {
+                    test: 2
+                },
                 resultMenuNonStomach: processMenuNames(bestMenuNonStomachArray)
-                
+
             }
-            
+
         });
     } else {
         postMessage({
             type: "not_found"
-        });        
+        });
         //return console.log("error");
     }
 }
